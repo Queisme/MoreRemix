ca39fa13b7b198f666870a964a2a426185a04334
Queisme <queiskey@gmail.com>
2024-10-18 12:51:26 -0500

    update

---
+++ FundMe.sol
@@ -0,0 +1,17 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimmum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+
+contract FundMe {
+    function fund() public {
+        // Allows users to send $
+        // Have a minimum $ 
+        
+    }
+
+    //function withdraw() public {}
+}
