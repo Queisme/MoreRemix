2a32ece951ef3794b64d2caba37d944ebfb26eef
Queisme <queiskey@gmail.com>
2024-10-19 18:59:05 -0500

    update

---
+++ contracts/FundMe.sol
@@ -0,0 +1,87 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimmum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+    function getPrice() internal view returns(uint256){
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
+
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 5e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor(){
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't seen enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        return priceFeed.version();
+    }
+
+    function withdraw() public onlyOwner {
+        // for loop
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // actually withdraw funds
+
+        // transfer
+        //payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        //bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        //require(sendSuccess, "Send Failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == i_owner, "Must be owner!");
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+}
