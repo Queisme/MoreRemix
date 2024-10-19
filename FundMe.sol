4ebf87391440fc96eac82dbf79e308769289d097
Queisme <queiskey@gmail.com>
2024-10-19 11:54:57 -0500

    update

--- FundMe.sol
+++ FundMe.sol
@@ -5,13 +5,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 
 contract FundMe {
-    function fund() public {
+
+    uint256 public minimumUSD = 5;
+
+    function fund() public payable {
         // Allows users to send $
-        // Have a minimum $ 
-        
+        // Have a minimum $ of $5.00
+        require(msg.value >= minimumUSD, "Didn't seen enough ETH");
+
     }
 
     //function withdraw() public {}
+
+    function getPrice() public view returns(uint256){
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate() public {}
 }
